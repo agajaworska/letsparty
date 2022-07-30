ea21c863b182038df3e7b64d78adcc057ce63d82
Agata Jaworska <agata.jaworska93@gmail.com>
2022-07-30 16:30:32 +0200

    Implement Item Model

---
+++ lib/models/item_models.dart
@@ -0,0 +1,11 @@
+class ItemModel {
+  final String adress;
+  final DateTime date;
+  final String id;
+
+  ItemModel({
+    required this.id,
+    required this.adress,
+    required this.date,
+  });
+}
