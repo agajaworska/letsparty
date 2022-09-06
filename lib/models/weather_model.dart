6db9375ca012958c8a1f8475bafdec5e78c38485
Agata Jaworska <agata.jaworska93@gmail.com>
2022-09-06 13:04:51 +0200

    Add .fromJson method to weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
+
 class WeatherModel {
   WeatherModel({
     required this.temperature,
     required this.city,
+    required this.condition,
   });
   final String city;
   final double temperature;
+  final String condition;
+
+  WeatherModel.fromJson(Map<String, dynamic> json)
+      : temperature = json['current']['temp_c'] + 0.0,
+        city = json['location']['name'],
+        condition = json['current']['condition']['text'];
 }
